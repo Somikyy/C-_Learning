ad8cbfbbc5b0b8e778ece16e526106dd6416fb98
LayBalt <somi.kyy@mail.ru>
2025-10-23 15:17:34 +0200

    More folders

---
+++ С++/helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int age = 25;
+    double height = 1.75;
+    std::string name = "Kirill";
+
+    std::cout << "I'm " << age << " years old" << std::endl;
+    std::cout << "My height is " << height << " meters" << std::endl;
+    std::cout << "My name is " << name << std::endl;
+
+    return 0;
+}
